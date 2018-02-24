04dbb6f0b616dc6c020262edac7059cbc68d2e0c
Kyle Dobby <kyledobby@Melissas-MBP.home>
2018-02-24 18:06:51 -0500

    fixed a few syntax errors

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BusinessList from './components/BusinessList';
-import SearchBar from './components/SearchBar';
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
 
 class App extends Component {
   render() {
--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./BusinessList.css";
-import Business from "./components/Business/Business.js"
+import Business from "../Business/Business.js"
 
 class BusinessList extends React.Component{
     render() {
