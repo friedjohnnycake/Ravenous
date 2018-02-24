e3434fec9c1cda238aaaf59b35af0cc0370d1177
Kyle Dobby <kyledobby@Melissas-MBP.home>
2018-02-24 18:57:38 -0500

    again, more fixes

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BusinessList from './components/BusinessList/BusinessList.js';
+import BusinessList from './components/BusinessList/BusinessList.jsnpm';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
 class App extends Component {
--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -19,7 +19,7 @@ return <li key={sortByOptionValue}>{sortByOption}</li>
         return <div className="SearchBar">
         <div className="SearchBar-sort-options">
           <ul>
-            {renderSortByOptions()}
+            {this.renderSortByOptions()}
           </ul>
         </div>
         <div className="SearchBar-fields">
