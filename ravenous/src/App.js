a372cdf21078dca59ced2864112d3d3316ab6c4a
Kyle Dobby <kyledobby@Melissas-MBP.home>
2018-02-25 16:18:15 -0500

    end of project 1

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BusinessList from './components/BusinessList/BusinessList.jsnpm';
+import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
 class App extends Component {
