c76978776b6de01650b4347c12e0e1a33c1c777c
Kyle Dobby <dobbyk2@gmail.com>
2018-03-09 19:02:39 -0500

    Ravenous pt 4

--- ravenous/src/util/Yelp.js
+++ ravenous/src/util/Yelp.js
@@ -4,25 +4,27 @@ const Yelp = {
         search (term, location, sortBy){
             return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: { {Authorization: `Bearer ${apiKey}`} }
                 }).then(response => {return response.json();
+                    console.log(response)
                  ).then(jsonResponse => {
                     if(jsonResponse.businesses){
-                        console.log(jsonResponse.businesses);
                         return jsonResponse.businesses.map(business => {
                             return {
-                                id:,
-                                imageSrc:,
-                                name:,
-                                address:,
-                                city:,
-                                state:,
-                                zipCode:,
-                                category:,
-                                rating:,
-                                reviewCount:,
+                                id: business.id,
+                                imageSrc: business.image_url,
+                                name: business.name,
+                                address: business.location.address1,
+                                city: business.location.city,
+                                state: business.location.state,
+                                zipCode: business.location.zip_code,
+                                category: business.categories[0].title,
+                                rating: business.rating,
+                                reviewCount: business.review_count,
                             },
                             console.log(jsonResponse.businesses)
                         });
             }
         });
     }
-}
+};
+
+export default Yelp;
